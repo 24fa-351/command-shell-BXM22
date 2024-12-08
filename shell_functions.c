c7117599c824925016c888e1e2a7737cdc75d515
BXM22 <brennen@csuf.fullerton.edu>
2024-12-07 20:50:47 -0800

    implemented symbols

--- shell_functions.c
+++ shell_functions.c
@@ -3,87 +3,143 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 #include "shell_functions.h"
+
 #define MAX_INPUT_SIZE 1024
 #define MAX_PATH_SIZE 1024
 
+void parse_arguments(const char *input, char **args) {
+    int i = 0;
+    char *token = strtok(strdup(input), " ");
+    while (token != NULL) {
+        args[i++] = token;
+        token = strtok(NULL, " ");
+    }
+    args[i] = NULL;
+}
 
+void handle_redirection(char **args) {
+    int in_fd = -1, out_fd = -1;
+    char *input_file = NULL;
+    char *output_file = NULL;
 
-void handle_command(int argc, char *argv[]){
+    for (int i = 0; args[i] != NULL; i++) {
+        if (strcmp(args[i], "<") == 0) {
+            input_file = args[i + 1];
+            args[i] = NULL;  // Remove the input redirection operator
+        } else if (strcmp(args[i], ">") == 0) {
+            output_file = args[i + 1];
+            args[i] = NULL;  // Remove the output redirection operator
+        }
+    }
+
+    if (input_file) {
+        in_fd = open(input_file, O_RDONLY);
+        if (in_fd < 0) {
+            perror("Failed to open input file");
+            exit(EXIT_FAILURE);
+        }
+        if (dup2(in_fd, STDIN_FILENO) < 0) {
+            perror("Failed to redirect input");
+            exit(EXIT_FAILURE);
+        }
+        close(in_fd);
+    }
+
+    if (output_file) {
+        out_fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (out_fd < 0) {
+            perror("Failed to open output file");
+            exit(EXIT_FAILURE);
+        }
+        if (dup2(out_fd, STDOUT_FILENO) < 0) {
+            perror("Failed to redirect output");
+            exit(EXIT_FAILURE);
+        }
+        close(out_fd);
+    }
+}
+
+void handle_pipes(char *command) {
+    char *cmds[2];
+    int pipe_fd[2];
+    pid_t pid;
+
+    cmds[0] = strtok(command, "|");
+    cmds[1] = strtok(NULL, "|");
+
+    if (cmds[1] == NULL) {
+        fprintf(stderr, "Invalid pipe command\n");
+        return;
+    }
+
+    if (pipe(pipe_fd) < 0) {
+        perror("Pipe creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((pid = fork()) == 0) {
+        close(pipe_fd[0]);
+        dup2(pipe_fd[1], STDOUT_FILENO);
+        close(pipe_fd[1]);
+        char *args[MAX_INPUT_SIZE];
+        parse_arguments(cmds[0], args);
+        execute_command(args);
+        exit(EXIT_SUCCESS);
+    }
+
+    if ((pid = fork()) == 0) {
+        close(pipe_fd[1]);
+        dup2(pipe_fd[0], STDIN_FILENO);
+        close(pipe_fd[0]);
+        char *args[MAX_INPUT_SIZE];
+        parse_arguments(cmds[1], args);
+        execute_command(args);
+        exit(EXIT_SUCCESS);
+    }
+
+    close(pipe_fd[0]);
+    close(pipe_fd[1]);
+    wait(NULL);
+    wait(NULL);
+}
+
+void execute_command(char **args) {
+    if (execvp(args[0], args) < 0) {
+        perror("Command execution failed");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void handle_command(int argc, char *argv[]) {
     if (argc < 1) {
         fprintf(stderr, "No command provided.\n");
         return;
     }
-    if (strcmp(argv[0], "cd") == 0) {
-        if (argc < 2) {
-            fprintf(stderr, "cd: missing argument\n");
-        } else if (chdir(argv[1]) == 0) {
-            printf("Changed directory to: %s\n", argv[1]);
-        } else {
-            perror("cd failed");
+
+    char *args[MAX_INPUT_SIZE];
+    parse_arguments(argv[0], args);
+
+    for (int i = 0; args[i] != NULL; i++) {
+        if (strcmp(args[i], "<") == 0 || strcmp(args[i], ">") == 0) {
+            handle_redirection(args); // Handle redirection here
+            break;
         }
     }
-    else if (strcmp(argv[0], "pwd") == 0) {
-        char cwd[MAX_PATH_SIZE];
-        if (getcwd(cwd, sizeof(cwd)) != NULL) {
-            printf("%s\n", cwd);
-        } else {
-            perror("pwd failed");
-        }
+
+    if (strchr(argv[0], '|') != NULL) {
+        handle_pipes(argv[0]);
+        return;
     }
-    else if (strcmp(argv[0], "set") == 0) {
-         if (argc < 3) {
-            fprintf(stderr, "set: missing argument(s)\n");
-        } else if (setenv(argv[1], argv[2], 1) == 0) {
-            printf("Set %s=%s\n", argv[1], argv[2]);
-        } else {
-            perror("set failed");
-        }
+
+    pid_t pid = fork();
+    if (pid == 0) {
+        execute_command(args);  // Execute command after handling redirection
+    } else if (pid > 0) {
+        wait(NULL);
+    } else {
+        perror("Fork failed");
     }
-    else if (strcmp(argv[0], "unset") == 0) {
-        if (argc < 2) {
-            fprintf(stderr, "unset: missing argument\n");
-        } else if (unsetenv(argv[1]) == 0) {
-            printf("Unset %s\n", argv[1]);
-        } else {
-            perror("unset failed");
-        }
-    }
-    else if (strcmp(argv[0], "echo") == 0) {
-        char buffer[MAX_INPUT_SIZE] = {0};
-        for (int i = 1; i < argc; i++) {
-        if (argv[i][0] == '$') {
-            char *var_name = argv[i] + 1; // Skip the '$'
-            char *value = getenv(var_name);
-            if (value) {
-                if (strlen(buffer) + strlen(value) + 1 < MAX_INPUT_SIZE) {
-                    strcat(buffer, value);
-                } else {
-                    fprintf(stderr, "echo: buffer overflow\n");
-                    return;
-                }
-            } else {
-                // Leave placeholder or empty space if the variable doesn't exist
-                if (strlen(buffer) + 1 < MAX_INPUT_SIZE) {
-                    strcat(buffer, " ");
-                } else {
-                    fprintf(stderr, "echo: buffer overflow\n");
-                    return;
-                }
-            }
-        } else {
-            if (strlen(buffer) + strlen(argv[i]) + 1 < MAX_INPUT_SIZE) {
-                strcat(buffer, argv[i]);
-            } else {
-                fprintf(stderr, "echo: buffer overflow\n");
-                return;
-            }
-        }
-        if (i < argc - 1) {
-            strcat(buffer, " ");
-        }
-    }
-    printf("%s\n", buffer);
-}
 }
--- shell_functions.h
+++ shell_functions.h
@@ -1,10 +1,11 @@
 #ifndef SHELL_FUNCTIONS_H
 #define SHELL_FUNCTIONS_H
 
-
-void run_comandline(void);
+void parse_arguments(const char *input, char **args);
+void handle_redirection(char **args);
+void handle_pipes(char *command);
+void execute_command(char **args);
 void handle_command(int argc, char *argv[]);
-
 
 
 #endif
